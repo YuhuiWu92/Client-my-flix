b9cd577bce9d1e036a829a792dc083c7c374312b
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-03-05 22:06:28 +0100

    setup registration

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,47 +1,89 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { RegistrationView } from "../registration-view/registration-view";
 
 class MainView extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            movies: [],
-            selectedMovie: null
-        };
-    }
-    componentDidMount(){
-      axios.get('https://my-film-flix.herokuapp.com/movies')
-        .then(response => {
-          this.setState({
-            movies: response.data
-          });
-        })
-        .catch(error => {
-          console.log(error);
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      selectedMovie: null,
+      user: null,
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://my-film-flix.herokuapp.com/movies")
+      .then((response) => {
+        this.setState({
+          movies: response.data,
         });
-    }
-    setSelectedMovie(newSelectedMovie) {
-      this.setState({
-        selectedMovie: newSelectedMovie
+      })
+      .catch((error) => {
+        console.log(error);
       });
-    }
+  }
 
-    render() {
-      const { movies, selectedMovie } = this.state;
-      if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+  /*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: movie,
+    });
+  }
+
+  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+  onRegister(newUser) {
+    this.setState({
+      newUser,
+    });
+  }
+
+  /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+  onLoggedIn(user) {
+    this.setState({
+      user,
+    });
+  }
+
+  render() {
+    const { movies, selectedMovie, user, newUser } = this.state;
+    if (!newUser)
       return (
+        <RegistrationView onRegister={(newUser) => this.onRegister(newUser)} />
+      );
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user)
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+
+    // Before the movies have been loaded
+    if (movies.length === 0) return <div className="main-view" />;
+    return (
       <div className="main-view">
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
-         ))
-        }
+        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
+        {selectedMovie ? (
+          <MovieView
+            movie={selectedMovie}
+            onBackClick={(newSelectedMovie) => {
+              this.setSelectedMovie(newSelectedMovie);
+            }}
+          />
+        ) : (
+          movies.map((movie) => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onMovieClick={(newSelectedMovie) => {
+                this.setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          ))
+        )}
       </div>
     );
   }
 }
- export default MainView; 
+export default MainView;
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
 export class MovieCard extends React.Component {
+  render() {
+    const { movie, onMovieClick } = this.props;
 
-    render() {
-        const { movie, onMovieClick } = this.props;
-
-        return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
-    }
+    return (
+      <div
+        className="movie-card"
+        onClick={() => {
+          onMovieClick(movie);
+        }}
+      >
+        {movie.Title}
+      </div>
+    );
+  }
 }
+// use propTypes to catch errors
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
