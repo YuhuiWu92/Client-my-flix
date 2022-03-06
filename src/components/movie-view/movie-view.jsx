b2aa27fda5ffc49844ab720d8b73583f72694157
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-03-06 11:12:04 +0100

    add prop Types

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,8 +1,8 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 export class MovieView extends React.Component {
-
-  // uncommen this to check the key presses 
+  // uncommen this to check the key presses
   /*  
   keypressCallback(event) {
     console.log(event.key);
@@ -15,10 +15,10 @@ export class MovieView extends React.Component {
   componentWillUnmount() {
     document.removeEventListener('keypress', this.keypressCallback);
   } */
-    render(){
-        const { movie } = this.props;
-        return(
-            <div className="movie-view">
+  render() {
+    const { movie } = this.props;
+    return (
+      <div className="movie-view">
         <div className="movie-poster">
           <img src={movie.ImagePath} />
         </div>
@@ -30,8 +30,37 @@ export class MovieView extends React.Component {
           <span className="label">Description: </span>
           <span className="value">{movie.Description}</span>
         </div>
-
-       </div>
-        );
-    } 
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <button
+          onClick={() => {
+            onBackClick(null);
+          }}
+        >
+          Back
+        </button>
+      </div>
+    );
+  }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+    }),
+    ImageUrl: PropTypes.string.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
+};
