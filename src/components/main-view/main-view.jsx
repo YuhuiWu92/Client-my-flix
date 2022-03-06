9a2f0b117d17e8ad753062e8cc0aa097bba08075
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-03-06 23:04:13 +0100

    set grid

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,7 +5,8 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { RegistrationView } from "../registration-view/registration-view";
-
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 class MainView extends React.Component {
   constructor() {
     super();
@@ -61,28 +62,32 @@ class MainView extends React.Component {
 
     // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
+
     return (
-      <div className="main-view">
-        {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
+      <Row className="main-view justify-content-md-center">
         {selectedMovie ? (
-          <MovieView
-            movie={selectedMovie}
-            onBackClick={(newSelectedMovie) => {
-              this.setSelectedMovie(newSelectedMovie);
-            }}
-          />
-        ) : (
-          movies.map((movie) => (
-            <MovieCard
-              key={movie._id}
-              movie={movie}
-              onMovieClick={(newSelectedMovie) => {
+          <Col md={8}>
+            <MovieView
+              movie={selectedMovie}
+              onBackClick={(newSelectedMovie) => {
                 this.setSelectedMovie(newSelectedMovie);
               }}
             />
+          </Col>
+        ) : (
+          movies.map((movie) => (
+            <Col md={3}>
+              <MovieCard
+                key={movie._id}
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => {
+                  this.setSelectedMovie(newSelectedMovie);
+                }}
+              />
+            </Col>
           ))
         )}
-      </div>
+      </Row>
     );
   }
 }
