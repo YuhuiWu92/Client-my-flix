33fe5d4e0a71eb1898643a123478ce0a03b36243
YuhuiWu92 <94534171+YuhuiWu92@users.noreply.github.com>
2022-03-06 18:30:22 +0100

    add card in moviecard

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Button } from "react-bootstrap/Button";
+import { Card } from "react-bootstrap/Card";
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
 
     return (
-      <div
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button
+            onClick={() => {
+              onMovieClick(movie);
+            }}
+            variant="primary"
+          >
+            Open
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
+{
+  /* <div
         className="movie-card"
         onClick={() => {
           onMovieClick(movie);
         }}
       >
         {movie.Title}
-      </div>
-    );
-  }
+      </div> */
 }
 // use propTypes to catch errors
 MovieCard.propTypes = {
